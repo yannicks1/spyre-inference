d07e23a0c618b187946bab4bd6863cfe35b7b002
Yannick Schnider <Yannick.Schnider1@ibm.com>
2026-07-24 17:20:48 +0200

    delete stale files
    Signed-off-by: Yannick Schnider <Yannick.Schnider1@ibm.com>

--- spyre_inference/custom_ops/__init__.py
+++ spyre_inference/custom_ops/__init__.py
@@ -14,7 +14,6 @@
 
 """This module contains all custom ops for spyre"""
 
-import os
 from functools import lru_cache
 
 from . import logits_processor  # noqa: F401
@@ -29,20 +28,9 @@ from vllm.logger import init_logger
 
 logger = init_logger(__name__)
 
-# RoPE kernel selector. Default is the slice-based kernel in rotary_embedding.py; set
-# SPYRE_ROPE_MATMUL=1 to swap in the equivalent matmul (contract) kernel from
-# rotary_embedding_matmul.py. The swap must happen before the model is built, so it is
-# done here in register_all (called from the worker's init_device, before load_model).
-_ROPE_USE_MATMUL = os.environ.get("SPYRE_ROPE_MATMUL", "0") == "1"
-
 
 @lru_cache(maxsize=1)
 def register_all():
     logger.info("Registering custom ops for spyre_inference")
     rotary_embedding.register()
-    if _ROPE_USE_MATMUL:
-        from . import rotary_embedding_matmul
-
-        rotary_embedding_matmul.use_matmul_rope()
-        logger.info("SPYRE_ROPE_MATMUL=1: swapped in matmul (contract) RoPE kernel")
     utils.register()
